d26e8f7d2051ea0cbba10423677759039aa82cc8
JasonStaubach <jbstaubach@gmail.com>
2023-05-17 13:09:31 -0400

    deep dup works

--- practice-for-ch-ruby-recursion-long-practice-main/recursion.rb
+++ practice-for-ch-ruby-recursion-long-practice-main/recursion.rb
@@ -24,3 +24,35 @@ def exp_two(b,n)
 end
 
 # p exp(11,590)
+
+
+class Array
+    def deep_dup
+        return [] if self.length == 0
+        return [self[0]] if !(self[0].is_a?(Array)) && self.length == 1
+        return [self[0]] + self[1..-1].deep_dup if !(self[0].is_a?(Array))
+        return [self[0].deep_dup] + self[1..-1].deep_dup
+    end
+end
+
+robot_parts = [
+  ["nuts", "bolts", "washers"],
+  ["capacitors", "resistors", "inductors"]
+]
+
+robot_parts_copy = robot_parts.deep_dup
+
+# shouldn't modify robot_parts
+
+robot_parts_copy[1] << "LEDs"
+# but it does
+# p robot_parts_copy
+p robot_parts_copy[1]
+p robot_parts[1]
+
+# arr = [[1,2],[3,5],[[6]]]
+# arr_two = arr.deep_dup
+# arr_two[1] << 4
+
+# p arr
+# p arr_two
